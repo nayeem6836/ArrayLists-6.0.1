f89d75c51787ca5d61cfcb4972fb3f53af8a7399
nayeem6836 <maseednayeem@gmail.com>
2024-08-12 20:09:22 +0530

    Update ForL.java

--- ForL.java
+++ ForL.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 public class ForL{
 public static void main(String[]args){
 // prepare an object for ArrayList
   // Load String instead of Integers 
   // Iterate them using for each loop
+
+ArrayList<String> a = new ArrayList<>();
+a.add("fayaz");
+a.add("sai");
+a.add("karthik");
+for(String s :a){
+  System.out.println(s);
+}
 }
 }
